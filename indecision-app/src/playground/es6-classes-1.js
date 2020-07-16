19ac0c79e21550a7bf65ba604933f0175d05c6a2
Jessica Menezes <jessiee.menezes@gmail.com>
2020-07-16 16:30:15 +0100

    learnt about parent and child classes

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -13,13 +13,55 @@ class Person {
     }
 
     getDescription(){
-        return `${this.name} is ${this.age} year(s) old`;
+        return `${this.name} is ${this.age} year(s) old.`;
     }
 }
 
- const me = new Person('Jessica', 22);
- //can access the methods in the individual instances of our class
- console.log(me.getDescription());
+//student class also contains stuff in Person
+class Student extends Person {
+   constructor(name, age, major){
+       //dont need to set this.name again
+       //super refers to the parent class, calls the parent constructor function
+       super(name, age); 
+       this.major = major; 
+   }
+ 
+   hasMajor(){
+       //if major has no string then !!'' = false because '' is a false value then double !! cancels out
+       return !!this.major;
+   }
 
- const other = new Person();
- console.log(other.getDescription());
+   //can over ride method from parent class
+   getDescription(){
+       let description = super.getDescription();
+
+       //checks if current student instance has this
+       if(this.hasMajor()){
+           description += ` Their major is ${this.major}.`; //same as description = description + ''; concatenating description
+       }
+       return description;
+   }
+}
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation){
+        super(name, age)
+        this.homeLocation = homeLocation;
+    }
+
+    getGreeting(){
+        let greeting = super.getGreeting();
+
+        if(this.homeLocation){
+            greeting += ` I am visiting from ${this.homeLocation}.`;
+        }
+        return greeting;
+    }
+}
+
+ const me = new Traveler('Jessica', 22, 'Macau');
+ //can access the methods in the individual instances of our class
+ console.log(me.getGreeting());
+
+ const other = new Traveler();
+ console.log(other.getGreeting());
